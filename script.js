0c81ef38fa7f81b82df81d40e1abcc232d8eb371
Gerardo Serrano <gerardoserrc@gmail.com>
2025-11-19 13:52:52 -0600

    feat: Implementación de funciones aritméticas en script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+let numero1 = prompt("Ingrese el primer número: ");
+let numero2 = prompt("Ingrese el segundo número: ");
+
+function sumaFuncion(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+function restaFuncion(numero1, numero2) {
+    return numero1 - numero2;
+}
+
+function multipliacionFuncion(numero1, numero2) {
+    return numero1 * numero2;
+}
+
+function divisionFuncion(numero1, numero2) {
+    return numero1 / numero2;
+}
+
+let suma = sumaFuncion(numero1, numero2);
+let resta = restaFuncion(numero1, numero2);
+let multipliacion = multipliacionFuncion(numero1, numero2);
+let division = divisionFuncion(numero1, numero2);
+
+console.log(suma);
+console.log(resta);
+console.log(multipliacion);
+console.log(division);
